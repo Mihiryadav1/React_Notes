b2ae7177049c6fe7782d6454e0267f0c81e7db48
MihirYadav <yadavmihir30@gmail.com>
2023-07-26 19:11:00 +0530

    Pre-Final Changes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 
 const Notes = (prop) => {
-    const { date, color } = prop;
-    //  document.querySelector('.grid-notes').style.backgroundColor="black"
-    //  const formattedDate = date ? date.toLocaleString() : '';
+    const { date,ondelete,task } = prop;
+   
     return (
         <>
 
             <div className="p-4">
                 <div className="grid-notes rounded d-flex flex-column justify-content-between" style={{ backgroundColor: prop.color }}>
-                    <p className="info">{prop.task}</p>
+                    <p className="info">{task}</p>
                     <div className="bottom-icons d-flex justify-content-between">
                         <div className="date">{date}</div>
-                        <div className="icon-delete"><i class="fa-solid fa-xmark"></i></div>
+                        <div className="icon-delete" onClick={ondelete}><i class="fa-solid fa-xmark"></i></div>
                     </div>
                 </div>
 
